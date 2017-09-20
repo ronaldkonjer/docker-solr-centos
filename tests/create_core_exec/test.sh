af04221642dba371781ff6d0b82c919a28ac7667
Martijn Koster <mak-github@greenhills.co.uk>
2017-09-20 23:42:09 +0100

    fix tests: encode space in URL

--- tests/create_core_exec/test.sh
+++ tests/create_core_exec/test.sh
@@ -33,7 +33,7 @@ echo "Loading data"
 docker exec --user=solr "$container_name" bin/post -c gettingstarted example/exampledocs/manufacturers.xml
 sleep 1
 echo "Checking data"
-data=$(docker exec --user=solr "$container_name" wget -q -O - 'http://localhost:8983/solr/gettingstarted/select?q=address_s%3ARound Rock')
+data=$(docker exec --user=solr "$container_name" wget -q -O - 'http://localhost:8983/solr/gettingstarted/select?q=address_s%3ARound%20Rock')
 if ! egrep -q 'One Dell Way Round Rock, Texas 78682' <<<$data; then
   echo "Test test_simple $tag failed; data did not load"
   exit 1

4d010eef9c34806028a65d716a769d436129f640
Martijn Koster <mak-github@greenhills.co.uk>
2017-09-21 10:44:43 +0100

    one more id change

--- tests/demo/test.sh
+++ tests/demo/test.sh
@@ -27,7 +27,7 @@ docker run --name "$container_name" -d "$tag" "solr-demo"
 wait_for_server_started "$container_name"
 
 echo "Checking data"
-data=$(docker exec --user=solr "$container_name" wget -q -O - 'http://localhost:8983/solr/demo/select?q=address_s%3ARound Rock')
+data=$(docker exec --user=solr "$container_name" wget -q -O - 'http://localhost:8983/solr/demo/select?q=id%3Adell')
 if ! egrep -q 'One Dell Way Round Rock, Texas 78682' <<<$data; then
   echo "Test $TEST_DIR $tag failed; data did not load"
   exit 1
